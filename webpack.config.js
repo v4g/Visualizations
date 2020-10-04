8c40e4287ce2751ac3dbd5bc449ee4ffa4629fe9
v4g <vinayak4gargya@gmail.com>
2020-10-03 23:10:43 -0400

    Created a base two.js app

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+    //     // { from: './src/css', to: './css' },
+        { from: './src/js/', to: './' },
+    //     // { from: './fonts/', to: './fonts' },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'ThreeJS',
+      template: 'src/index.html'
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.css'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    minimize: true,
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+    // minimizer: [new TerserPlugin({
+    //   // include: /\.js/,
+    // })],
+  },
+
+};
